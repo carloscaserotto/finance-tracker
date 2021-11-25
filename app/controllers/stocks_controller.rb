aabeaa063a1c35204c8345b45e331421d380b684
carlos <carlos.caserotto@yahoo.com>
2021-11-25 12:48:48 -0300

    Add js responses

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,26 @@
+class StocksController < ApplicationController
+    
+    def search
+        #byebug
+        if params[:stock].present?
+            @stock = Stock.new_lookup(params[:stock])
+            #render json: @stock
+            if @stock
+                respond_to do |format|
+                    format.js { render partial: 'users/result'}
+                end
+            else
+                respond_to do |format|
+                    flash.now[:alert]="Please enter a valid symbol"
+                    format.js { render partial: 'users/result'}
+                end
+                
+            end
+        else
+            respond_to do |format|
+                flash.now[:alert]="Please enter symbol"
+                format.js { render partial: 'users/result'}
+            end
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
+ devise_for :users
  root 'welcome#index'
+
+ get 'my_porfolio', to: 'users#my_porfolio'
+ get 'search_stock', to: 'stocks#search'
 end
